685af65cd7fa0a81d632a99316ce68e8e7cdaf37
Mojie <mojiefong@gmail.com>
2023-07-09 11:20:10 +0800

    chore: 配置全局颜色

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,9 @@ page {
 
   --background-color-base: #f5f7fa;
 
-  --box-shadow-base: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
+  --box-shadow-base: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+
+  color: var(--color-text-base);
+  background: var(--background-color-base);
 }
 </style>
