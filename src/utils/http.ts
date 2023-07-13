3b335a0c9ddee3546da21a15a82ffbd46067c97a
Mojie <mojiefong@gmail.com>
2023-07-13 23:30:03 +0800

    chore: 添加类型

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -4,9 +4,19 @@
  */
 
 type Options = UniNamespace.RequestOptions
+type Response<T = any> = UniNamespace.RequestSuccessCallbackResult & {
+  data?: T
+}
+type HttpPromise<T = any> = Promise<Response<T>>
 
-export class Http {
-  request(options: Options) {
+uni.addInterceptor('request', {
+  invoke(args) {
+    args.url = import.meta.env.VITE_BASE_URL + args.url
+  },
+})
+
+class Http {
+  request<T = any>(options: Options): HttpPromise<T> {
     return new Promise((resolve, reject) => {
       uni.request({
         url: options.url,
@@ -14,7 +24,7 @@ export class Http {
         header: options.header,
         data: options.data,
         success: (res) => {
-          resolve(res)
+          resolve(res as unknown as HttpPromise<T>)
         },
         fail: (err) => {
           reject(err)
@@ -23,27 +33,27 @@ export class Http {
     })
   }
 
-  get(url: string, config?: Options) {
+  get<T = any>(url: string, config?: Partial<Options>): HttpPromise<T> {
     return this.request({ url, method: 'GET', ...config })
   }
 
-  delete(url: string, config?: Options) {
+  delete<T = any>(url: string, config?: Partial<Options>): HttpPromise<T> {
     return this.request({ url, method: 'DELETE', ...config })
   }
 
-  head(url: string, config?: Options) {
+  head<T = any>(url: string, config?: Partial<Options>): HttpPromise<T> {
     return this.request({ url, method: 'HEAD', ...config })
   }
 
-  options(url: string, config?: Options) {
+  options<T = any>(url: string, config?: Partial<Options>): HttpPromise<T> {
     return this.request({ url, method: 'OPTIONS', ...config })
   }
 
-  post(url: string, data: Options['data'], config?: Options) {
+  post<T = any>(url: string, data: Options['data'], config?: Partial<Options>): HttpPromise<T> {
     return this.request({ url, method: 'POST', data, ...config })
   }
 
-  put(url: string, data: Options['data'], config?: Options) {
+  put<T = any>(url: string, data: Options['data'], config?: Partial<Options>): HttpPromise<T> {
     return this.request({ url, method: 'PUT', data, ...config })
   }
 }
