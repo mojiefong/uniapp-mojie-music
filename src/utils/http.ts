1ecd42bcff52da8d65e6e19ef88c64eb8b8116b0
Mojie <mojiefong@gmail.com>
2023-07-13 09:24:44 +0800

    refactor: http request

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -5,32 +5,48 @@
 
 type Options = UniNamespace.RequestOptions
 
-function http(options: Options) {
-  return new Promise((resolve, reject) => {
-    uni.request({
-      url: options.url,
-      method: options.method,
-      header: options.header,
-      data: options.data,
-      success: (res) => {
-        resolve(res)
-      },
-      fail: (err) => {
-        reject(err)
-      },
+export class Http {
+  request(options: Options) {
+    return new Promise((resolve, reject) => {
+      uni.request({
+        url: options.url,
+        method: options.method,
+        header: options.header,
+        data: options.data,
+        success: (res) => {
+          resolve(res)
+        },
+        fail: (err) => {
+          reject(err)
+        },
+      })
     })
-  })
+  }
+
+  get(url: string, config?: Options) {
+    return this.request({ url, method: 'GET', ...config })
+  }
+
+  delete(url: string, config?: Options) {
+    return this.request({ url, method: 'DELETE', ...config })
+  }
+
+  head(url: string, config?: Options) {
+    return this.request({ url, method: 'HEAD', ...config })
+  }
+
+  options(url: string, config?: Options) {
+    return this.request({ url, method: 'OPTIONS', ...config })
+  }
+
+  post(url: string, data: Options['data'], config?: Options) {
+    return this.request({ url, method: 'POST', data, ...config })
+  }
+
+  put(url: string, data: Options['data'], config?: Options) {
+    return this.request({ url, method: 'PUT', data, ...config })
+  }
 }
 
-['delete', 'get', 'head', 'options'].forEach((method: string) => {
-  http.prototype[method] = function (url: string, config?: Options) {
-    return http({ url, method: 'GET', ...config })
-  }
-})
-
-;['post', 'put'].forEach((method: string) => {
-  http.prototype[method] = function (url: string, data: Options['data'], config?: Options) {
-    const _method = method as Options['method']
-    return http({ url, method: _method, data, ...config })
-  }
-})
+const http = new Http()
+export { http }
