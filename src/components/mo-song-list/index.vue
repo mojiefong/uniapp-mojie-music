570401046645ab4dd163e8772cab0c1d6c7e1c7e
Mojie <mojiefong@gmail.com>
2023-07-16 18:49:17 +0800

    chore: 迁移样式

--- src/components/mo-song-list/index.vue
+++ src/components/mo-song-list/index.vue
@@ -16,3 +16,15 @@ import type { SongList } from '@/models/user'
 
 defineProps<{ songList: SongList[] }>()
 </script>
+
+<style scoped>
+.name {
+  font-size: 22rpx;
+  padding-top: 16rpx;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+}
+</style>
