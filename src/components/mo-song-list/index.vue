f831d3b67c7a1345585028244f5f9bc59fe1d627
Mojie <mojiefong@gmail.com>
2023-08-05 21:57:28 +0800

    feat: 新增唱针和cd页面

--- src/components/mo-song-list/index.vue
+++ src/components/mo-song-list/index.vue
@@ -2,7 +2,7 @@
   <view class="grid grid-cols-3 gap-2 pt-2">
     <view v-for="item in songList" :key="item.id" @click="emit('click', item)">
       <view class="w-full h-29vw">
-        <image class="w-full h-full rd-2" :src="`${item.picUrl}?param=200y200`" />
+        <image class="wh-full rd-2" :src="`${item.picUrl}?param=200y200`" />
       </view>
       <text class="name">
         {{ item.name }}
--- src/pages/player/player.vue
+++ src/pages/player/player.vue
@@ -1,7 +1,7 @@
 <template>
-  <view class="w-full h-full fixed top-0 left-0 bg-black">
-    <view class="w-full h-full absolute top-0 left-0 opacity-50 blur-[20px] -z1">
-      <image class="w-full h-full" :src="currentSong.album?.picUrl" />
+  <view class="wh-full fixed top-0 left-0 bg-black">
+    <view class="wh-full absolute top-0 left-0 opacity-50 blur-[20px] -z1">
+      <image class="wh-full" :src="`${currentSong.album?.picUrl}?param=400y400`" />
     </view>
 
     <view class="w-full h-9 text-sm absolute top-0 left-0 z-10 text-white">
@@ -13,6 +13,18 @@
         {{ currentSong.singers?.join('/') }}
       </view>
     </view>
+
+    <view class="w-70 relative pt-30 mxa">
+      <view
+        class="w-18.5 h-30 absolute left-49% top-15 z-100
+        bg-[url(~@/static/stylus.png)] bg-cover bg-no-repeat
+        rotate-0 origin-[12px_12px] transition-transform duration-0.3s"
+        :class="{ '-rotate-30!': isOk }"
+      />
+      <view class="w-full h-70 bg-[url(~@/static/circle.png)] bg-cover bg-no-repeat relative">
+        <image class="w-64% h-64% rd-50% pos-center" :src="`${currentSong.album?.picUrl}?param=200y200`" />
+      </view>
+    </view>
   </view>
 </template>
 
@@ -20,6 +32,7 @@
 import { onLoad } from '@dcloudio/uni-app'
 import { usePlayer } from '@/store/player'
 
+const isOk = ref(true)
 const { currentSong } = usePlayer()
 
 onLoad(() => {
